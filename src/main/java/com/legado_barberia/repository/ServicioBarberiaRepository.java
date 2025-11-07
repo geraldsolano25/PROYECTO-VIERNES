a3f9137e4cf81c929d2f8df9e7590226f7e3fff4
Faquesada2210 <114116664+Faquesada2210@users.noreply.github.com>
2025-11-06 22:08:27 -0600

    Create ServicioBarberiaRepository.java

---
+++ src/main/java/com/legado_barberia/repository/ServicioBarberiaRepository.java
@@ -0,0 +1,4 @@
+package com.legado_barberia.repository;
+import com.legado_barberia.domain.ServicioBarberia;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ServicioBarberiaRepository extends JpaRepository<ServicioBarberia,Long>{}
